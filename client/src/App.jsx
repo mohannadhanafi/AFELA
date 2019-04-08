504806565953f5fab23dccce8e528e12d3fd18d2
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-04-08 14:18:37 +0300

    we

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-filename-extension */
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {}
+
+  render() {
+    return (
+      <h1>This is our project</h1>
+    );
+  }
+}
+
+export default App;
