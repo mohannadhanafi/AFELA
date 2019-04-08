f4664069da1299f7374075d1a86b10135719dda7
HemaSAli <34215823+HemaSAli@users.noreply.github.com>
2019-04-08 14:35:33 +0300

    m

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/jsx-filename-extension */
+
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="App" />
+    );
+  }
+}
+
+export default App;
