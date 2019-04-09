a3ed2e07aedc35379c842431a7709783f56082bf
lubnaabad <lubna_mosa@hotmail.com>
2019-04-09 12:11:02 +0300

    add galleries

--- client/src/components/common/Ads/index.jsx
+++ client/src/components/common/Ads/index.jsx
@@ -8,7 +8,7 @@ export default class Ads extends Component {
   render() {
     return (
       <div className="widget ad-336x280 text-center">
-        <Link to="#">
+        <Link to="/">
           <img src="http://deothemes.com/envato/afela/html/img/magazine/336_ad.jpg" alt="" className="image__Ads" />
         </Link>
       </div>
