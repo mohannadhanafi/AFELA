77f4643e82851f14b1b68e51c40caa3935874a39
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-04-09 12:09:51 +0300

    s

---
+++ client/src/components/common/Layouts/EditorPicks/index.jsx
@@ -0,0 +1,120 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-multiple-empty-lines */
+import React, { Component } from 'react';
+
+export default class index extends Component {
+    state = {}
+
+    render() {
+      return (
+        <section className="section-wrap relative pb-70 pt-0">
+          <div className="row">
+            <div className="col-sm-12">
+              <h2 className="heading relative heading-small uppercase bottom-line style-2 left-align">editors picks</h2>
+              <ul className="posts-list no-top-pad clearfix">
+
+                <li className="clearfix">
+                  <article>
+                    <div className="entry-img hover-scale">
+                      <a href="magazine-single-article.html" className="entry-category-label blue">World</a>
+                      <a href="magazine-single-article.html">
+                        <img src="img/magazine/15.jpg" alt="" />
+                      </a>
+                    </div>
+                    <div className="entry">
+                      <h2 className="entry-title"><a href="magazine-single-article.html">The Golden Age: How the PR Industry Is Coming Into its Own</a></h2>
+                      <ul className="entry-meta list-inline">
+                        <li className="entry-date">
+                          <a href="#">19 Dec, 2015</a>
+                        </li>
+                        <li className="entry-author">
+                          <i className="fa fa-user" />
+                          <a href="#">John Doe</a>
+                        </li>
+                        <li className="entry-comments">
+                          <i className="fa fa-comments" />
+                          <a href="magazine-single-article.html">15</a>
+                        </li>
+                      </ul>
+                      <div className="entry-content">
+                        <p>We possess within us two minds. So far I have written only of the conscious mind. Involve them in new product launches and keep them "in the loop. Ironically, scheduling breaks and walks outside actually helps you.</p>
+                        <a href="magazine-single-article.html" className="read-more dark-link">Read More<i className="fa fa-angle-right" />
+                        </a>
+                      </div>
+                    </div>
+                  </article>
+                </li>
+
+                <li className="clearfix">
+                  <article>
+                    <div className="entry-img hover-scale">
+                      <a href="magazine-single-article.html" className="entry-category-label violet">Tech</a>
+                      <a href="magazine-single-article.html">
+                        <img src="img/magazine/16.jpg" alt="" />
+                      </a>
+                    </div>
+                    <div className="entry">
+                      <h2 className="entry-title"><a href="magazine-single-article.html">Indiana Governor Signs Religious Freedom Law, Sparking Debates That Echo the Hobby Lobby Dispute</a></h2>
+                      <ul className="entry-meta list-inline">
+                        <li className="entry-date">
+                          <a href="#">19 Dec, 2015</a>
+                        </li>
+                        <li className="entry-author">
+                          <i className="fa fa-user" />
+                          <a href="#">John Doe</a>
+                        </li>
+                        <li className="entry-comments">
+                          <i className="fa fa-comments" />
+                          <a href="magazine-single-article.html">15</a>
+                        </li>
+                      </ul>
+                      <div className="entry-content">
+                        <p>We possess within us two minds. So far I have written only of the conscious mind. Involve them in new product launches and keep them "in the loop. Ironically, scheduling breaks and walks outside actually helps you.</p>
+                        <a href="magazine-single-article.html" className="read-more dark-link">Read More
+                          <i className="fa fa-angle-right" />
+                        </a>
+                      </div>
+                    </div>
+                  </article>
+                </li>
+
+                <li className="clearfix">
+                  <article>
+                    <div className="entry-img hover-scale">
+                      <a href="magazine-single-article.html" className="entry-category-label green">Travel</a>
+                      <a href="magazine-single-article.html">
+                        <img src="img/magazine/17.jpg" alt="" />
+                      </a>
+                    </div>
+                    <div className="entry">
+                      <h2 className="entry-title"><a href="magazine-single-article.html">The One Simple Communications Solution That Can Help Your Startup Win Against the Big Guys</a></h2>
+                      <ul className="entry-meta list-inline">
+                        <li className="entry-date">
+                          <a href="#">19 Dec, 2015</a>
+                        </li>
+                        <li className="entry-author">
+                          <i className="fa fa-user" />
+                          <a href="#">John Doe</a>
+                        </li>
+                        <li className="entry-comments">
+                          <i className="fa fa-comments" />
+                          <a href="magazine-single-article.html">15</a>
+                        </li>
+                      </ul>
+                      <div className="entry-content">
+                        <p>We possess within us two minds. So far I have written only of the conscious mind. Involve them in new product launches and keep them "in the loop. Ironically, scheduling breaks and walks outside actually helps you.</p>
+                        <a href="magazine-single-article.html" className="read-more dark-link">Read More
+                          <i className="fa fa-angle-right" />
+                        </a>
+                      </div>
+                    </div>
+                  </article>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </section>
+      );
+    }
+}
