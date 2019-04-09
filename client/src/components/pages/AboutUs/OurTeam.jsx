01228c9e54dd10b9170498e8961153caedc4988b
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-04-09 16:29:25 +0300

    weR

--- client/src/components/pages/AboutUs/OurTeam.jsx
+++ client/src/components/pages/AboutUs/OurTeam.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-export default function OurTeam() {
+export default function OurTeam({ teamTitle, team }) {
   return (
     <section className="section-wrap-lg our-team pb-80">
       <div className="container">
@@ -10,107 +10,45 @@ export default function OurTeam() {
           <div className="col-md-6 col-md-offset-3 text-center">
             <h2 className="bottom-line">Our Team</h2>
             <p className="subheading style-2">
-              Creative Minds With The Extraordinary Skills
+              {teamTitle}
             </p>
           </div>
         </div>
 
         <div className="row">
-          <div
-            className="col-md-4 col-sm-6 team-wrap mb-40 wow fadeInUp"
-            data-wow-duration="1.2s"
-            data-wow-delay=".1s"
-          >
-            <div className="team-member text-center">
-              <div className="team-img" onClick="return true">
-                <img src="http://deothemes.com/envato/afela/html/img/team_2.jpg" alt="" />
-                <div className="overlay">
-                  <div className="team-details text-center">
-                    <div className="social-icons rounded">
-                      <a href="#">
-                        <i className="fab fa-facebook" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-google-plus" />
-                      </a>
-                      <a href="#">
-                        <i className="fa fa-envelope" />
-                      </a>
+          {team.map(element => (
+            <div
+              className="col-md-4 col-sm-6 team-wrap mb-40 wow fadeInUp"
+              data-wow-duration="1.2s"
+              data-wow-delay=".1s"
+            >
+              <div className="team-member text-center">
+                <div className="team-img" onClick="return true">
+                  <img src={element.image} alt="" />
+                  <div className="overlay">
+                    <div className="team-details text-center">
+                      <div className="social-icons rounded">
+                        <a href={element.facebook}>
+                          <i className="fab fa-facebook" />
+                        </a>
+                        <a href={element.twitter}>
+                          <i className="fab fa-twitter" />
+                        </a>
+                        <a href={element.google}>
+                          <i className="fab fa-google-plus" />
+                        </a>
+                        <a href={element.email}>
+                          <i className="fa fa-envelope" />
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
+                <h4 className="team-title">{element.name}</h4>
+                <span>{element.jobTitle}</span>
               </div>
-              <h4 className="team-title">David Geller</h4>
-              <span>CEO of Company</span>
             </div>
-          </div>
-
-          <div
-            className="col-md-4 col-sm-6 team-wrap mb-40 wow fadeInUp"
-            data-wow-duration="1.2s"
-            data-wow-delay=".2s"
-          >
-            <div className="team-member text-center">
-              <div className="team-img">
-                <img src="http://deothemes.com/envato/afela/html/img/team_2.jpg" alt="" />
-                <div className="overlay">
-                  <div className="team-details text-center">
-                    <div className="social-icons rounded">
-                      <a href="#">
-                        <i className="fab fa-facebook" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-google-plus" />
-                      </a>
-                      <a href="#">
-                        <i className="fa fa-envelope" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <h4 className="team-title">Monica Bing</h4>
-              <span>Art Director</span>
-            </div>
-          </div>
-
-          <div
-            className="col-md-4 col-sm-6 team-wrap wow fadeInUp"
-            data-wow-duration="1.2s"
-            data-wow-delay=".3s"
-          >
-            <div className="team-member last text-center">
-              <div className="team-img">
-                <img src="http://deothemes.com/envato/afela/html/img/team_2.jpg" alt="" />
-                <div className="overlay">
-                  <div className="team-details text-center">
-                    <div className="social-icons rounded">
-                      <a href="#">
-                        <i className="fab fa-facebook" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-twitter" />
-                      </a>
-                      <a href="#">
-                        <i className="fab fa-google-plus" />
-                      </a>
-                      <a href="#">
-                        <i className="fa fa-envelope" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <h4 className="team-title">Joey Tribbiani</h4>
-              <span>Lead Designer</span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
--- client/src/components/pages/AboutUs/index.jsx
+++ client/src/components/pages/AboutUs/index.jsx
@@ -4,14 +4,46 @@ import WhatWeDo from './WhatWeDo';
 import OurTeam from './OurTeam';
 
 export default class AboutUs extends Component {
-  state = {};
+  state = {
+    teamTitle: 'Creative Minds With The Extraordinary Skills',
+    team: [
+      {
+        name: 'David Geller',
+        jobTitle: 'CEO of Company',
+        image: 'http://deothemes.com/envato/afela/html/img/team_2.jpg',
+        facebook: 'facebook',
+        twitter: 'twitter',
+        google: 'google',
+        email: 'email',
+      },
+      {
+        name: 'Monica Bing',
+        jobTitle: 'Art Director',
+        image: 'http://deothemes.com/envato/afela/html/img/team_2.jpg',
+        facebook: 'facebook',
+        twitter: 'twitter',
+        google: 'google',
+        email: 'email',
+      },
+      {
+        name: 'Joey Tribbiani',
+        jobTitle: 'Lead Designer',
+        image: 'http://deothemes.com/envato/afela/html/img/team_2.jpg',
+        facebook: 'facebook',
+        twitter: 'twitter',
+        google: 'google',
+        email: 'email',
+      },
+    ],
+  };
 
   render() {
+    const { teamTitle, team } = this.state;
     return (
       <>
         <TopSection />
         <WhatWeDo />
-        <OurTeam />
+        <OurTeam teamTitle={teamTitle} team={team} />
       </>
     );
   }
