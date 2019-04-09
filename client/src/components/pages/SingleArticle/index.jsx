f13a9a9ae9898d7578aef1297f909184178fd88d
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-04-09 15:39:23 +0300

    we

--- client/src/components/pages/SingleArticle/index.jsx
+++ client/src/components/pages/SingleArticle/index.jsx
@@ -13,6 +13,7 @@ import Tags from '../../common/Tags';
 import PostBody from './Post';
 import RelatedPosts from './RelatedPosts';
 import Comments from './Comments';
+import AddComment from './AddComment';
 
 export default class SigleArtice extends Component {
   state = {
@@ -103,6 +104,7 @@ export default class SigleArtice extends Component {
                 />
                 <RelatedPosts relatedPosts={relatedPosts} />
                 <Comments AllComments={AllComments} comments={comments} />
+                <AddComment />
               </div>
               <aside className="col-md-4 sidebar pb-50">
                 <FollowUs />
