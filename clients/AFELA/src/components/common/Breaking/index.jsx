0e10e7e317204c935e41ff3f1da66c7f3d365a1f
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-04-10 11:12:23 +0300

    s

--- clients/AFELA/src/components/common/Breaking/index.jsx
+++ clients/AFELA/src/components/common/Breaking/index.jsx
@@ -3,6 +3,9 @@
 /* eslint-disable react/jsx-closing-tag-location */
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import moment from 'moment';
+import uuid from 'uuid';
 
 export default class index extends Component {
   state = {
@@ -20,24 +23,33 @@ export default class index extends Component {
     ],
   };
 
+  componentDidMount() {
+    axios('/api/v1/getRelatedPosts').then((result) => {
+      const { data } = result;
+      this.setState({ breaking: data });
+    });
+  }
+
+
   render() {
     const { breaking } = this.state;
     return (
       <div className="container">
-      <div className="breaking-news mt-30 clearfix">
-        <span className="uppercase">Breaking News:</span>
-        <div id="ticker" className="flexslider">
-          <ul className="slides clearfix">
-            {breaking.map(value => (
-              <li>
-                <Link to={value.link}>
-                  <time>{value.time}</time>
-                  {` ${value.text}`}</Link>
-              </li>
-            ))}
-          </ul>
+        <div className="breaking-news mt-30 clearfix">
+          <span className="uppercase">Breaking News:</span>
+          <div id="ticker" className="flexslider">
+            <ul className="slides clearfix">
+              {breaking.map(value => (
+                <li key={uuid()}>
+                  {/* <Link to={`/news/${value.category.seo}/${value.seo}`}> */}
+                  {/* <time>{moment(value.createdAt).format('HH:mm')}</time> */}
+                  {` ${value.title}` || null}
+                  {/* </Link> */}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
       </div>
     );
   }
