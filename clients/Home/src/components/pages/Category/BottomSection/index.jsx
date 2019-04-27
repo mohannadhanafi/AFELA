52e66158ec2a9093c0654f1c2b1cdd810c9492f6
lubnaabad <lubna_mosa@hotmail.com>
2019-04-27 13:42:02 +0300

    nn

--- clients/Home/src/components/pages/Category/BottomSection/index.jsx
+++ clients/Home/src/components/pages/Category/BottomSection/index.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Popular from '../../../common/Popular';
 import Galleries from '../../../common/Galleries';
 import Category from '../../../common/Category';
 import Tags from '../../../common/Tags';
-import Continer from './Continer';
+import LayoutOne from './LayoutOne';
+import LayoutTwo from './LayoutTwo';
+
+
 import './style.css';
 
-export default function index({ posts, total, changeData }) {
-  return (
-    <div className="container margin-content">
-      <div className="row">
-        <div className="col-md-8 post-content mb-50 ">
-          <Continer posts={posts} changeData={changeData} total={total} />
+class index extends Component {
+  state={}
+
+  setLayout = (layout) => {
+    const { posts, total, changeData } = this.props;
+
+    switch (layout) {
+      case 'One':
+        return <LayoutOne posts={posts} changeData={changeData} total={total} />;
+      case 'Two':
+        return <LayoutTwo posts={posts} changeData={changeData} total={total} />;
+      default:
+        return null;
+    }
+  };
+
+  render() {
+    return (
+      <div className="container margin-content">
+        <div className="row">
+          <div className="col-md-8 post-content mb-50 ">
+            { this.setLayout('One')}
+
+          </div>
+          <aside className="col-md-4 sidebar pb-50">
+            <Category />
+            <Popular />
+            <Galleries />
+            <Tags />
+          </aside>
         </div>
-        <aside className="col-md-4 sidebar pb-50">
-          <Category />
-          <Popular />
-          <Galleries />
-          <Tags />
-        </aside>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default index;
