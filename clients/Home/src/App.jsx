c0e2fefa227c1961189f1b01e96da875d443eec7
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-05-08 15:51:43 +0300

    s

--- clients/Home/src/App.jsx
+++ clients/Home/src/App.jsx
@@ -43,7 +43,6 @@ class App extends Component {
         </Router>
         <Footer />
         <BackToTop />
-
       </div>
     );
   }
--- clients/Home/src/store.js
+++ clients/Home/src/store.js
@@ -4,9 +4,7 @@ import logger from 'redux-logger';
 
 import reducers from './appRedux/reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(
-  applyMiddleware(thunk, logger),
-));
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, applyMiddleware(thunk));
 
 export default store;
